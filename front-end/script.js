63fb1777215a3175944a5e5a0c4383ac47d35b1a
Si mohamed <m.lamhaouas@gmail.com>
2021-09-08 13:51:33 -0400

    getProduct function done

--- front-end/script.js
+++ front-end/script.js
@@ -1,13 +1,43 @@
 // get the DATA from the server using fetch and check for the response ok status
+function getProducts() {
+  fetch('http://localhost:3000/api/teddies')
+    .then(response => {
+      console.log(response)
+      // check for errors
+      if (!response.ok) {
+        throw Error();
+      }
+      return response.json();
+    })
+    .then((data) => {
 
-fetch('http://localhost:3000/api/teddies')
-  .then(response => {
-    if (!response.ok) {
-      throw Error();
-    }
-    return response.json();
-  })
-  .then(data => console.log(data))
-  .catch(error => {
-    alert('server is down')
-  });
+      console.log(data);
+
+      const html = data
+        .map(teddies => {
+          return `
+          <div class="product" id="product">
+          <a href="">
+            <div class="card" style="width: 18rem;">
+              <img src="${teddies.imageUrl}" class="card-img-top" alt="${teddies.name}">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${teddies.name}</h5>
+            <p class="card-text">${teddies.description}</p>
+            <p>${teddies.price}$</p>
+            <a href="${teddies.name}" class="btn btn-primary">Add to cart</a>
+          </div>
+        </div>
+        `;
+        }).join('');
+
+      document.querySelector('#product').insertAdjacentHTML("afterbegin", html);
+
+    })
+
+    .catch(error => {
+      alert('server is down')
+    });
+}
+
+getProducts()
