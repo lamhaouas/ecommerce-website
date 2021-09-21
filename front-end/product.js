59f2d7dd024b8ff5dd30ce5743f021284afb491c
Si mohamed <m.lamhaouas@gmail.com>
2021-09-20 20:05:00 -0400

    creatProduct function with javascript

--- front-end/product.js
+++ front-end/product.js
@@ -17,25 +17,55 @@ function showProduct() {
             return response.json();
         })
         .then((data) => {
+            console.log(data)
             // creat single product using jQuery append
             const teddy = data;
-            console.log(teddy);
-            $('#product').append(`
-            <img class="col-lg-6" src="${teddy.imageUrl}" alt="" >
-            <div class="col-lg-6">
-                <h1>${teddy.name}</h1>
-                <p>${teddy.description}</p>
-                <form id="productForm" action="">
-                    <label id="productLabel" for="">Color</label>
-                    <select name="" id="">
-                        <option value="">${teddy.colors[0]}</option>
-                        <option value="">${teddy.colors[1]}</option>
-                        <option value="">${teddy.colors[2]}</option>
-                        <option value="">${teddy.colors[3]}</option>
-                    </select>
-                </form>
-                <button class=" btn btn-outline-primary" id="addToCart">Add to cart</button>
-            `);
+
+            function createProduct() {
+
+                const productImage = document.getElementById('product-img');
+                const productTiltle = document.getElementById('product-title');
+                const productDesc = document.getElementById('product-description');
+                const optionOne = document.getElementById('option-0');
+                const optionTwo = document.getElementById('option-1');
+                const optionThree = document.getElementById('option-2');
+                const optionFour = document.getElementById('option-3');
+
+                productImage.setAttribute('src', teddy.imageUrl);
+                productTiltle.innerHTML = teddy.name;
+                productDesc.innerHTML = teddy.description;
+                optionOne.innerHTML = teddy.colors[0];
+                optionTwo.innerHTML = teddy.colors[1];
+                optionThree.innerHTML = teddy.colors[2];
+                optionFour.innerHTML = teddy.colors[3];
+            }
+            // call the creat product function
+            createProduct()
+
+            //-----------------------------------------------------------------------------------------------------------
+
+            // creat single product using jQuery append
+            // const teddy = data;
+            // console.log(teddy);
+            // $('#product').append(`
+            // <img class="col-lg-6" src="${teddy.imageUrl}" alt="" >
+            // <div class="col-lg-6">
+            //     <h1>${teddy.name}</h1>
+            //     <p>${teddy.description}</p>
+            //     <form id="productForm" action="">
+            //         <label id="productLabel" for="">Color</label>
+            //         <select name="" id="">
+            //             <option value="">${teddy.colors[0]}</option>
+            //             <option value="">${teddy.colors[1]}</option>
+            //             <option value="">${teddy.colors[2]}</option>
+            //             <option value="">${teddy.colors[3]}</option>
+            //         </select>
+            //     </form>
+            //     <button class=" btn btn-outline-primary" id="addToCart">Add to cart</button>
+            // `);
+            //-----------------------------------------------------------------------------------------------------------
+
+
         })
         .catch(error => {
             document.getElementById("product").innerHTML = "Please try later, Thank you!";
